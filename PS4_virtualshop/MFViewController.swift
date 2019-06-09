6b12f6594d4ed4cc4860b9243c239b6f5b4b7e7d
davis <davis@davisde-Mac.local>
2019-06-09 01:13:37 -0700

    123

---
+++ PS4_virtualshop/MFViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MFViewController.swift
+//  PS4_virtualshop
+//
+//  Created by davis on 2019/6/9.
+//  Copyright © 2019 Providence Honor School. All rights reserved.
+//
+
+import UIKit
+
+class MFViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
